416141af9181cc09d4fc33822df7363a2c09de58
tokizuoh <tokizuoh@gmail.com>
2023-07-09 15:30:26 +0900

    Add JSON deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use serde::{Deserialize, Serialize};
+use std::{fs::File, io::BufReader};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Project {
+    pub name: String,
+    pub display_name: String,
+    pub exported: i64,
+    pub users: Vec<User>,
+    pub pages: Vec<Page>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub id: String,
+    pub name: String,
+    pub display_name: String,
+    pub email: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Page {
+    pub title: String,
+    pub created: i64,
+    pub updated: i64,
+    pub id: String,
+    pub lines: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = "tokizuoh-public.json";
+
+    let file = File::open(file_name).unwrap();
+    let reader = BufReader::new(file);
+
+    let deserialized: Project = serde_json::from_reader(reader).unwrap();
+
+    for page in deserialized.pages {
+        println!("{}, {}", page.title, page.created);
+    }
 }
